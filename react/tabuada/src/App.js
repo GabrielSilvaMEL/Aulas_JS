d182b196f1d701d409f85ac898f9d42c598441e5
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-10-17 21:25:49 -0300

    Update App.js

--- react/tabuada/src/App.js
+++ react/tabuada/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Titulo from './Titulo';
 import Titulo2 from './Titulo2';
 import Tabuada from './Tabuada';
-let n = parseInt(prompt("Informe um número","5"));
 
 function App() {
   return (
