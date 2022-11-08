1855bdf837352207109af35d2b099a6bb0d972c6
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-11-07 21:27:20 -0300

    up

--- react/tabuada/src/Calculadora.js
+++ react/tabuada/src/Calculadora.js
@@ -1,19 +1,52 @@
 import React from 'react';
 
+  const [input, setInput] = useState('');
 
+  const agregarInput = val => {
+    setInput(input + val);
+  };
 
-//   <button>8</button>
-//   <button>9</button>
-//   <button>4</button>
-//   <button>5</button>
-//   <button>6</button>
-//   <button>1</button>
-//   <button>2</button>
-//   <button>3</button>
-//   <button>0</button>
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("O valor não é válido.");
+    }
+  };
 
-
-function Titulo2(){
-    var display = 0;
-
-}
+  return (
+    <div className='App'>
+      <div className='calculadora'>
+        <Pantalla input={input}/>
+        <div className='fila'>
+          <button funcClic={agregarInput}>1</button>
+          <button funcClic={agregarInput}>2</button>
+          <button funcClic={agregarInput}>3</button>
+          <button funcClic={agregarInput}>+</button>
+        </div>
+        <div className='fila'>
+          <button funcClic={agregarInput}>4</button>
+          <button funcClic={agregarInput}>5</button>
+          <button funcClic={agregarInput}>6</button>
+          <button funcClic={agregarInput}>-</button>
+        </div>
+        <div className='fila'>
+          <button funcClic={agregarInput}>7</button>
+          <button funcClic={agregarInput}>8</button>
+          <button funcClic={agregarInput}>9</button>
+          <button funcClic={agregarInput}>*</button>
+        </div>
+        <div className='fila'>
+          <button funcClic={calcularResultado}>=</button>
+          <button funcClic={agregarInput}>0</button>
+          <button funcClic={agregarInput}>.</button>
+          <button funcClic={agregarInput}>/</button>
+        </div>
+        <div className='fila'>
+          <buttonClear funcClear={() => setInput('')}>
+            C
+          </buttonClear>
+        </div>
+      </div>
+    </div>
+  );
